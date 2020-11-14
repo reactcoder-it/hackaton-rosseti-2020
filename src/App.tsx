f3ea41a19e704a7fe9970babc34e286325af4d0e
forestcrahser <forestcrasher@outlook.com>
2020-11-14 18:17:31 +0300

    Set tasks as start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import { ROUTES } from './constants/routes'
 import { withSession } from './hooks/session'
 
 import PrivateRoute from './components/PrivateRoute'
 import NotFound from './containers/NotFound'
-import Home from './containers/Home'
 import Login from './containers/Login'
 import Register from './containers/Register'
 import Task from './containers/Task'
 import Tasks from './containers/Tasks'
 
 function App() {
+  const redirectTo = (to: string) => () => <Redirect to={to} />
   return (
     <Router>
       <Switch>
-        <PrivateRoute exact={true} path={ROUTES.HOME} component={Home} />
+        <PrivateRoute exact={true} path={ROUTES.HOME} component={redirectTo(ROUTES.TASKS)} />
         <PrivateRoute exact={true} path={ROUTES.TASK} component={Task} />
         <PrivateRoute exact={true} path={ROUTES.TASKS} component={Tasks} />
         <Route exact={true} path={ROUTES.LOGIN} component={Login} />
